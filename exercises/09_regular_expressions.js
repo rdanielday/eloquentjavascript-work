d3da757a066e7abc7058d80684f77b5086ad5ab7
Ryan Day <rdanielday@gmail.com>
2017-07-14 21:07:01 +0000

    regex exercises

--- exercises/09_regular_expressions.js
+++ exercises/09_regular_expressions.js
@@ -4,39 +4,44 @@ function verify(regexp, yes, no) {
   yes.forEach(function(s) {
     if (!regexp.test(s))
       console.log("Failure to match '" + s + "'");
+    else console.log("Good");
   });
   no.forEach(function(s) {
     if (regexp.test(s))
       console.log("Unexpected match for '" + s + "'");
+    else
+      console.log("Good");
   });
 }
 
 // Fill in the regular expressions
 
-verify(/.../,
+verify(/ca[tr]/,
        ["my car", "bad cats"],
        ["camper", "high art"]);
 
-verify(/.../,
+verify(/pr?op/,
        ["pop culture", "mad props"],
        ["plop"]);
 
-verify(/.../,
+verify(/^ferr[aey]/,
        ["ferret", "ferry", "ferrari"],
        ["ferrum", "transfer A"]);
 
-verify(/.../,
+verify(/ious\b/,
        ["how delicious", "spacious room"],
        ["ruinous", "consciousness"]);
 
-verify(/.../,
+verify(/\s[.,:;]/,
        ["bad punctuation ."],
        ["escape the dot"]);
 
-verify(/.../,
+verify(/\b\w[^\s]{6,}$/,
        ["hottentottententen"],
        ["no", "hotten totten tenten"]);
 
-verify(/.../,
+verify(/(\b[^e\s]+\b)/gi,
        ["red platypus", "wobbling nest"],
        ["earth bed", "learning ape"]);
+
+console.log(/(\b[^e\s]+\b)/gi.test("wobbling nest"));
